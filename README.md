4b42351abfe2dd90402c46a478f1b1b2f31beedf
shivishbrahma <pur.cho.99@gmail.com>
2020-01-17 18:34:52 +0530

    v 1.0.2

--- README.md
+++ README.md
@@ -1,33 +1,53 @@
 # geetishroban
+
 A jQuery and html based music playing app with a python scraper and youtube downloader
 
 ## Features
-*	Playlist (switch to any song on double click)
-* 	Play, Pause and Step Back and Forward on click and Next and Prev Song on double click
-*	Volume increase and decrease
+
+- Playlist (switch to any song on double click)
+
+- Play, Pause and Step Back and Forward on click and Next and Prev Song on double click
+
+- Volume increase and decrease
+
+- Added keyboard shortcuts
+
+## Keyboard ShortCuts
+
+| Shortcuts           | Action                           |
+| ------------------- | -------------------------------- |
+| Direction Key Up    | Previous song in playlist        |
+| Direction Key Down  | Next song in playlist            |
+| Direction Key Left  | Step 10s backward song duration  |
+| Direction Key Right | Step 10s forward song duration   |
+| Enter key           | Play or pause a song             |
+| Key V               | Volume up by 10%                 |
+| Key M               | Volume down by 10%               |
+| Key R               | Repeat in loop or song or normal |
 
 ## Steps
 
-* Clone the entire repository
+- Clone the entire repository
 
-	` git clone https://github.com/shivishbrahma/geetishroban`
+  `git clone https://github.com/shivishbrahma/geetishroban`
 
-* Install the requirements of the python (Python 3)
-	
-	``` 
-	cd geetishroban
-	pip install -r requirements.txt
-	```
-*	Download the songs using songscraper.py
-	
-	` python songscraper.py`
+- Install the requirements of the python (Python 3)
 
-	[Note : You can install any other songs by changing songs array where you insert the song search queries. Here I used my own blog... [Geetilipi](http://geetilipi.tk), so many songs are not there, kindly check there.]
+  `cd geetishroban`
 
-*	Run the index.html in your browser.
+  `pip install -r -I requirements.txt`
+
+- Download the songs using songscraper.py
+
+  `python songscraper.py`
+
+  [Note : You can install any other songs by changing songs array where you insert the song search queries. Here I used my own blog... [Geetilipi](http://geetilipi.tk), so many songs are not there, kindly check there.]
+
+- Run the index.html in your browser.
 
 ## New Features to be added
-*	Lyrics play along with the songs
-*	New themes for the player
-*	Player skins
-*	Playlist add and delete options
+
+- Lyrics play along with the songs
+- New themes for the player
+- Player skins
+- Playlist add and delete options
--- js/function.js
+++ js/function.js
@@ -1,172 +1,122 @@
-// Player Object
-const player = {};
-// Set the index to the player
-player.index = 0;
-player.loop = 0;
-// Load audio element
-player.song = $('audio').get(0);
-// Load Song of Index (index)
-player.loadSong = () => {
-  $('.playlist .list-group-item').removeClass('active');
-  $($('.playlist .list-group-item').get(player.index)).addClass('active');
-  player.song.src = songs[player.index]['src'];
-  $('.picture').attr('src', songs[player.index]['img']);
-  $('.metadata').text(
-    songs[player.index]['title'] + ' - ' + songs[player.index]['artist']
-  );
-  player.song.loop = false;
-  player.song.autoplay = true;
-  player.song.load();
-};
-//Load Playlist
-player.loadPlaylist = () => {
-  const playlist = $('.playlist');
-  songs.forEach((item, index) => {
-    text = item['title'] + ' - ' + item['artist'];
-    songListItem = $('<div></div>').text('');
-    span = $('<span></span>').text(text);
-    songListItem.append(span);
-    songListItem.attr('class', 'marquee-list');
-    divwrap = $('<li></li>').text('');
-    divwrap.attr('class', 'list-group-item bg-transparent');
-    divwrap.append(songListItem);
-    divwrap.dblclick(() => {
-      player.index = index;
-      player.loadSong();
+$(() => {
+  // Player Object
+  const player = {};
+  //Setting containers
+  player.container = {
+    playpause: $('#play-pause'),
+    volumeSlider: $('#volume-slider'),
+    musicSlider: $('#music-slider'),
+    picture: $('#picture'),
+    playlist: $('#playlist'),
+    repeat: $('#repeat'),
+    forward: $('#step-forward'),
+    backward: $('#step-backward')
+  };
+  player.fn = {};
+  // Set the index to the player
+  player.index = 0;
+  player.loop = 0;
+  // Load audio element
+  player.song = $('audio').get(0);
+
+  // Convert seconds to Time
+  convertSecToTime = s => {
+    let date = new Date(null);
+    date.setSeconds(s); // specify value for SECONDS here
+    return date.toISOString().substr(11, 8);
+  };
+
+  // Load Song of Index (index)
+  player.fn.loadSong = () => {
+    player.container.playlist
+      .children('.list-group-item')
+      .removeClass('active');
+    $(
+      player.container.playlist.children('.list-group-item').get(player.index)
+    ).addClass('active');
+    player.song.src = songs[player.index]['src'];
+    player.container.picture.attr('src', songs[player.index]['img']);
+    $('.metadata').text(
+      songs[player.index]['title'] + ' - ' + songs[player.index]['artist']
+    );
+    player.song.loop = false;
+    player.song.autoplay = true;
+    player.song.load();
+  };
+
+  //Load Playlist
+  player.fn.loadPlaylist = () => {
+    songs.forEach((item, index) => {
+      text = item['title'] + ' - ' + item['artist'];
+      songListItem = $('<div></div>').text('');
+      span = $('<span></span>').text(text);
+      songListItem.append(span);
+      songListItem.attr('class', 'marquee-list');
+      divwrap = $('<li></li>').text('');
+      divwrap.attr('class', 'list-group-item bg-transparent');
+      divwrap.append(songListItem);
+      divwrap.dblclick(() => {
+        player.index = index;
+        player.fn.loadSong();
+      });
+      player.container.playlist.append(divwrap);
     });
-    playlist.append(divwrap);
-  });
-};
-// Play pause
-player.playPause = () => {
-  $(event.target).toggleClass('fa-play');
+  };
 
-  $(event.target).toggleClass('fa-pause');
-  if (player.song.paused) {
-    player.song.play();
-    // console.log(song.textTracks);
-  } else {
-    player.song.pause();
-  }
-};
-// Convert seconds to Time
-convertSecToTime = s => {
-  let date = new Date(null);
-  date.setSeconds(s); // specify value for SECONDS here
-  return date.toISOString().substr(11, 8);
-};
-// Go to the next song
-player.nextSong = () => {
-  player.index = (player.index + 1) % songs.length;
-  player.loadSong();
-  // player.song.play();
-};
-// Go to the prev song
-player.prevSong = () => {
-  player.index = (player.index + songs.length - 1) % songs.length;
-  player.loadSong();
-  // player.song.play();
-};
+  // Play pause
+  player.fn.playPause = () => {
+    player.container.playpause.toggleClass('fa-play');
 
-$(document).ready(() => {
-  player.loadPlaylist();
-  player.loadSong();
-  let shift = 0;
-  // console.log($('.metadata').innerWidth());
-
-  width = $('.metadata').innerWidth() + 10;
-  $('.marquee').css('transform', `translateX(${width / 2}px)`);
-  const marquee = anime({
-    targets: '.marquee',
-    autoplay: true,
-    delay: 0,
-    translateX: -width,
-    duration: width * 55,
-    loop: true,
-    easing: 'linear'
-  });
-  const marqueeList = anime({
-    targets: '.marquee-list',
-    autoplay: true,
-    delay: (el, i, l) => {
-      return i * 200;
-    },
-    endDelay: (el, i, l) => {
-      return (l - i) * 200;
-    },
-    translateX: -width,
-    duration: width * 55,
-    loop: true,
-    easing: 'linear'
-  });
-  $('.marquee-list').hover(
-    () => {
-      marqueeList.restart();
-      marqueeList.pause();
-    },
-    () => {
-      marqueeList.play();
+    player.container.playpause.toggleClass('fa-pause');
+    if (player.song.paused) {
+      player.song.play();
+      // console.log(song.textTracks);
+    } else {
+      player.song.pause();
     }
-  );
-  $('audio').bind('loadeddata', () => {
-    $('.play-pause').addClass('fa-play');
-    $('.play-pause').removeClass('fa-pause');
-  });
-  $('audio').bind('ended', () => {
-    $('.play-pause').addClass('fa-play');
-    $('.play-pause').removeClass('fa-pause');
-    if (player.loop <= 1) {
-      if (!(player.index == songs.length - 1 && player.loop == 0))
-        player.nextSong();
-    }
-  });
-  $('audio').bind('playing', () => {
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  });
-  $('audio').bind('timeupdate', () => {
-    const element = event.target;
-    $('.volume-slider').val(element.volume);
-    $('.music-slider').val(element.currentTime);
-    $('.start-time').text(convertSecToTime(element.currentTime));
-    if (!isNaN(element.duration)) {
-      $('.music-slider').attr('max', element.duration);
-      $('.end-time').text(convertSecToTime(element.duration));
-    }
-  });
-  if (!player.song.canPlayType('audio/mpeg')) {
-    $('.msg-txt').removeClass('alert-success');
-    $('.msg-txt').addClass('alert-danger');
-    $('.msg-txt').text("Can't play mp3");
-  } else {
-    $('.msg-txt').addClass('alert-success');
-    $('.msg-txt').removeClass('alert-danger');
-    $('.msg-txt').text('Can play mp3');
-  }
-  $('.play-pause').click(player.playPause);
-  $('.music-slider').on('input', () => {
-    player.song.currentTime = $(event.target).val();
-  });
+  };
 
-  $('.volume-slider').on('input', () => {
-    player.song.volume = $(event.target).val();
-  });
-  $('.fa-step-backward').click(() => {
-    player.song.currentTime -= 10;
-  });
-  $('.fa-step-forward').click(() => {
+  // Go to the next song
+  player.fn.nextSong = () => {
+    player.index = (player.index + 1) % songs.length;
+    player.fn.loadSong();
+    // player.song.play();
+  };
+  // Go to the prev song
+  player.fn.prevSong = () => {
+    player.index = (player.index + songs.length - 1) % songs.length;
+    player.fn.loadSong();
+    // player.song.play();
+  };
+  // Step forward
+  player.fn.stepForward = () => {
     player.song.currentTime += 10;
-  });
+  };
+  // Step backward
+  player.fn.stepBackward = () => {
+    player.song.currentTime -= 10;
+  };
 
-  $('.fa-step-backward').dblclick(() => {
-    player.prevSong();
-  });
-  $('.fa-step-forward').dblclick(() => {
-    player.nextSong();
-  });
-  $('.fa-repeat').click(() => {
+  // Volume up
+  player.fn.volumeUp = () => {
+    if (player.song.volume < 1) {
+      player.song.volume = player.song.volume + 0.1;
+      player.container.volumeSlider.val(player.song.volume);
+    }
+  };
+
+  // Volume down
+  player.fn.volumeDown = () => {
+    if (player.song.volume > 0) {
+      player.song.volume = player.song.volume - 0.1;
+      player.container.volumeSlider.val(player.song.volume);
+    }
+  };
+
+  // Repeat
+  player.fn.repeat = () => {
     player.loop = (player.loop + 1) % 3;
-    className = $(event.target).attr('class');
+    className = player.container.repeat.attr('class');
     className = className.replace('gray-loop', '').replace('green-loop', '');
     if (player.loop == 2) {
       player.song.loop = true;
@@ -175,6 +125,134 @@ $(document).ready(() => {
       player.song.loop = false;
       if (player.loop == 1) className = className + ' green-loop';
     }
-    $(event.target).attr('class', className);
+    player.container.repeat.attr('class', className);
+  };
+
+  $(document).ready(() => {
+    player.fn.loadPlaylist();
+    player.fn.loadSong();
+    let shift = 0;
+    // console.log($('.metadata').innerWidth());
+
+    width = $('.metadata').innerWidth() + 10;
+    $('.marquee').css('transform', `translateX(${width / 2}px)`);
+    const marquee = anime({
+      targets: '.marquee',
+      autoplay: true,
+      delay: 0,
+      translateX: -width,
+      duration: width * 55,
+      loop: true,
+      easing: 'linear'
+    });
+    const marqueeList = anime({
+      targets: '.marquee-list',
+      autoplay: true,
+      delay: (el, i, l) => {
+        return i * 200;
+      },
+      endDelay: (el, i, l) => {
+        return (l - i) * 200;
+      },
+      translateX: -width,
+      duration: width * 55,
+      loop: true,
+      easing: 'linear'
+    });
+    $('.marquee-list').hover(
+      () => {
+        marqueeList.restart();
+        marqueeList.pause();
+      },
+      () => {
+        marqueeList.play();
+      }
+    );
+    // Init Player
+    $('audio').bind('loadeddata', () => {
+      player.container.playpause.addClass('fa-play');
+      player.container.playpause.removeClass('fa-pause');
+    });
+    $('audio').bind('ended', () => {
+      player.container.playpause.addClass('fa-play');
+      player.container.playpause.removeClass('fa-pause');
+      if (player.loop <= 1) {
+        if (!(player.index == songs.length - 1 && player.loop == 0))
+          player.fn.nextSong();
+      }
+    });
+    $('audio').bind('playing', () => {
+      player.container.playpause.removeClass('fa-play');
+      player.container.playpause.addClass('fa-pause');
+    });
+    // Update with progress
+    $('audio').bind('timeupdate', () => {
+      const element = event.target;
+      player.container.volumeSlider.val(element.volume);
+      player.container.musicSlider.val(element.currentTime);
+      $('.start-time').text(convertSecToTime(element.currentTime));
+      if (!isNaN(element.duration)) {
+        player.container.musicSlider.attr('max', element.duration);
+        $('.end-time').text(convertSecToTime(element.duration));
+      }
+    });
+
+    if (!player.song.canPlayType('audio/mpeg')) {
+      $('.msg-txt').removeClass('alert-success');
+      $('.msg-txt').addClass('alert-danger');
+      $('.msg-txt').text("Can't play mp3");
+    } else {
+      $('.msg-txt').addClass('alert-success');
+      $('.msg-txt').removeClass('alert-danger');
+      $('.msg-txt').text('Can play mp3');
+    }
+
+    player.container.playpause.click(player.fn.playPause);
+    player.container.musicSlider.on('input', () => {
+      player.song.currentTime = $(event.target).val();
+    });
+
+    player.container.volumeSlider.on('input', () => {
+      player.song.volume = $(event.target).val();
+    });
+
+    player.container.backward.click(player.fn.stepBackward);
+    player.container.forward.click(player.fn.stepForward);
+
+    player.container.backward.dblclick(player.fn.prevSong);
+
+    player.container.forward.dblclick(player.fn.nextSong);
+
+    player.container.repeat.click(player.fn.repeat);
+  });
+
+  // Shortcut keys
+  $(document).bind('keydown', evt => {
+    switch (evt.keyCode) {
+      case 13:
+        player.fn.playPause(); // Enter Keypress
+        break;
+      case 37:
+        player.fn.stepBackward(); // Left Direction Key
+        break;
+      case 38:
+        player.fn.prevSong(); // Up Direction keypress
+        break;
+      case 39:
+        player.fn.stepForward(); // Right Direction Keypress
+        break;
+      case 40:
+        player.fn.nextSong(); // Down Direction keypress
+        break;
+      case 77:
+        player.fn.volumeDown(); // Key M press
+        break;
+      case 82:
+        player.fn.repeat(); // Key R press
+        break;
+      case 86:
+        player.fn.volumeUp(); // Key V press
+        break;
+    }
   });
 });
